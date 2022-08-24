2b2145c2e8553881a4271af65e8afaf2b5bb292f
ohansck <ohansck@gmail.com>
2022-08-24 16:23:21 +0100

    typescript variables mod

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
 import { requireAuth, generateJWT } from './auth'
@@ -31,12 +32,12 @@ const path = require('path');
 
   /**************************************************************************** */
 
-  app.get('/filteredimage', requireAuth, async (req, res) => {
+  app.get('/filteredimage', requireAuth, async (req: Request, res: Response) => {
     let image_URL: any = req.query.image_url;
     //image_URL is type 'any' beacuse req.query.image_url can be string or object
     //Takes the first image url in the event of same multiple queries
     if (typeof image_URL === 'object') {
-      image_URL = image_URL[0]
+      image_URL = image_URL[0];
     }
 
     //Check if query 'imageQual' exists
@@ -46,7 +47,7 @@ const path = require('path');
     if (qualityString) {
       quality = parseInt(qualityString.toString());
       if (quality < 10) { quality = 10; }
-      else if (quality >= 100) { quality = 100 }
+      else if (quality >= 100) { quality = 100; }
     } else {
       quality = 60;
     }
@@ -54,49 +55,52 @@ const path = require('path');
     if (image_URL) {
       if (image_URL !== '') {
         //Prevents processing imageQual query as part of image url
-        let image__URL: string = image_URL.split('&imageQual=')[0]
+        let image__URL: string = image_URL.split('&imageQual=')[0];
         await filterImageFromURL(image__URL, quality)
           //if promise resolved
           .then((filterImagePath) => {
             res.status(200).sendFile(filterImagePath, () => {
-              deleteLocalFiles([filterImagePath])
+              deleteLocalFiles([filterImagePath]);
             })
           })
           //if promise rejected
           .catch((error) => {
             //console.log(error.message)
-            res.status(404).send(`Invalid Query Value. Could not fetch resource. Check image URL`)
+            // Handles error when 1) HTTP URL is invalid
+            // 2) If target URL is unreachable
+            // 3) If filterImageFromURL throws an error, rejecting the promise
+            res.status(404).send(`Invalid Query Value. Could not fetch resource. Check image URL`);
           })
       } else {
         //if image URL empty
-        res.status(406).send('Please enter image URL')
+        res.status(406).send('Please enter image URL');
       }
     } else {
       //if image_url query does not exist
-      res.status(400).send('Invalid Query Key. Try GET /filteredimage?image_url={{}}&imageQual={{1-100}}')
+      res.status(400).send('Invalid Query Key. Try GET /filteredimage?image_url={{}}&imageQual={{1-100}}');
     }
   })
 
   //Login, where token is generated from body parameters
-  app.post('/login', (req, res) => {
+  app.post('/login', (req: Request, res: Response) => {
     const { name, role } = req.body;
     const tokenJWT: string = generateJWT({ name: name, role: role });
-    res.set('Authorisation', `Bearer ${tokenJWT}`)
-    res.send({ name: name, role: role, token: tokenJWT })
+    res.set('Authorisation', `Bearer ${tokenJWT}`);
+    res.send({ name: name, role: role, token: tokenJWT });
   })
   //! END @TODO1
 
   // Root Endpoint
   // Displays a simple message to the user. Edited to add new query
-  app.get("/", (req, res) => {
+  app.get("/", (req: Request, res: Response) => {
 
-    res.send("try GET /filteredimage?image_url={{}}&imageQual={{1-100}}")
+    res.send("try GET /filteredimage?image_url={{}}&imageQual={{1-100}}");
 
   });
 
   //Added by me: Ohaneme Kingsley. To redirect all other request types.
-  app.use((req, res) => {
-    res.redirect('/')
+  app.use((req: Request, res: Response) => {
+    res.redirect('/');
   })
   // Start the Server
   app.listen(port, () => {
